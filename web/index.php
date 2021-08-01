224868e983944c83fbfc4aeb81a0afd5f068540e
camilovietnam <camilovietnam@gmail.com>
2021-08-01 21:29:29 +0700

    refactor code

--- web/index.php
+++ web/index.php
@@ -11,38 +11,27 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $docs_handler = __DIR__ . '/../../bashupload-docs/index.php';
 $action = 'default';
 
-if ( in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT']) ) {
+if (in_array($_SERVER['REQUEST_METHOD'], ['POST', 'PUT'])) {
   $action = 'upload';
-} else {
-  # load documentation, if we have the docs repo cloned
-  // if ( is_file($docs_handler) ) {
-  //   $has_docs = true;
-  //   $doc = include $docs_handler;
-  //   if ( $doc ) {
-  //     $action = 'docs';
-  //   }
-  // }
-  
-  // # everything else is a possible file to download
-  if ( !isset($doc) && ($uri != BASE_WEB) ) {
+} elseif ($uri != BASE_WEB) {
     $action = 'file';
-  }
 }
-
-
 
 # Execute routed handler
 $accept = explode(',', $_SERVER['HTTP_ACCEPT']);
-if ( isset($_POST['json']) && $_POST['json'] == 'true' ) $renderer = 'json';
-else if ( in_array('text/html', $accept) ) $renderer = 'html';
-else $renderer = 'txt';
 
-$action_handler = __DIR__ . "/../actions/{$action}.php";
-if ( is_file($action_handler) ) {
-  include $action_handler;
+if (isset($_POST['json']) && $_POST['json'] == 'true') {
+    $renderer = 'json';
+} elseif (in_array('text/html', $accept)) {
+    $renderer = 'html';
+} else {
+    $renderer = 'txt';
 }
 
-
+$action_handler = __DIR__ . "/../actions/{$action}.php";
+if (is_file($action_handler)) {
+  include $action_handler;
+}
 
 # Render
 include __DIR__ . "/../render/{$renderer}.php";
